d0e79af764ec8fd57aadd1c7732121eccef6760d
Wenqian Zhao <zhwq11308@sina.com>
2019-11-21 00:55:05 +0800

    fix bug regarding previous posts

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ const PRECACHE_LIST = [
   "./js/snackbar.js",
   // "./img/icon_wechat.png",
   "./img/dabao.jpg",
-  "./img/home-bg.jpg",
+  "./img/automl.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
   "./css/bootstrap.min.css"
